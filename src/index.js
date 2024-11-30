67bba57142328b513ff6a3bea2190410f30d3942
omareldalil24 <omareldalil24@gmail.com>
2024-12-01 00:33:46 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // تأكد من المسار الصحيح
-serviceWorkerRegistration.register(); // تسجيل الـ Service Worker
+import reportWebVitals from './reportWebVitals';
 
-
-// تسجيل الـ Service Worker
-if (process.env.NODE_ENV === 'production') {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker
-        .register('/serviceWorker.js') // تأكد من أن المسار صحيح
-        .then((registration) => {
-          console.log('Service Worker registered with scope: ', registration.scope);
-        })
-        .catch((error) => {
-          console.log('Service Worker registration failed: ', error);
-        });
-    });
-  }
-}
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// تسجيل الـ service worker
+serviceWorkerRegistration.register();
 
 reportWebVitals();
